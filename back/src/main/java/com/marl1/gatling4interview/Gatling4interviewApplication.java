914e15deb831c33efa2ebefb7e8f07f018aa5ca3
marl1 <levequemc@gmail.com>
2025-07-26 12:52:06 +0200

    spring boot working

---
+++ back/src/main/java/com/marl1/gatling4interview/Gatling4interviewApplication.java
@@ -0,0 +1,13 @@
+package com.marl1.gatling4interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gatling4interviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gatling4interviewApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/marl1/gatling4interview/Gatling4interviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marl1.gatling4interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gatling4interviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
